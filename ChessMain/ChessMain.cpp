cc6d1be25de0cdd0f2c485d84da8d64060cfe6e7
valdeezzee <jvalderrama91@knights.ucf.edu>
2017-01-13 00:57:41 -0500

    Adding Chess cpp

---
+++ ChessMain/ChessMain.cpp
@@ -0,0 +1,12 @@
+// ChessMain.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "hello world" << endl;
+    return 0;
+}
+
